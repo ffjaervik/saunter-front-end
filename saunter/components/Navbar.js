63fab864152e26f5697c399fb88f906c28501f04
Emily9903 <emily_9903@hotmail.com>
2022-07-26 12:06:08 +0100

    feat: homepage updated with image, more navbar layout completed

--- saunter/components/Navbar.js
+++ saunter/components/Navbar.js
@@ -1,15 +1,15 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import "../styles/Navbar.module.css"
+import styles from "../styles/Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className={styles.navbar}>
       <Link href="/"><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
       <Link href="/community/"><a>Community</a></Link>
       <Link href="/contact/"><a>Contact</a></Link>
-      <button className="btn" href="/login/"><a>Login</a></button>
+      <button className="btn" href="/login/">Login</button>
     </nav>
   );
 }
--- saunter/pages/index.js
+++ saunter/pages/index.js
@@ -1,10 +1,15 @@
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Homepage.module.css'
+import {Picture1} from "../public/assets/Picture1.jpg"
+import "../public/assets/homepageimg.webp"
 
 export default function Home() {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div classname={styles.homepage}>
+      <Image src="/../public/assets/homepageimg.webp" height={100} width={100} alt="homepage image" />
+      <h1>Welcome!</h1>
+      <h4>Take the stress out of solo travelling</h4>
+      <button className="btn">Start Here</button>
     </div>
   )
 }
