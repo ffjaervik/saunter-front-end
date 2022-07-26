8b526fcdb5f0648b4a15f1e5b8213db16f502da6
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-07-26 11:01:46 +0100

    style: globals css styling and moving Navbar styling to Navbar.module.css

--- saunter/components/Navbar.js
+++ saunter/components/Navbar.js
@@ -1,14 +1,15 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import "../styles/Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="navbar">
       <Link href="/"><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
       <Link href="/community/"><a>Community</a></Link>
       <Link href="/contact/"><a>Contact</a></Link>
-      <button href="/login/"><a>Login</a></button>
+      <button className="btn" href="/login/"><a>Login</a></button>
     </nav>
   );
 }
