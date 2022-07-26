f9fe0271c6948c8aed977a479be7058c4b2933bb
May <MogoolMayaB@users.noreply.github.com>
2022-07-26 14:01:22 +0100

    feat: background image and nav bar displaying

--- saunter/pages/index.js
+++ saunter/pages/index.js
@@ -5,11 +5,13 @@ import "../public/assets/homepageimg.webp"
 
 export default function Home() {
   return (
-    <div classname={styles.homepage}>
-      <Image src="/../public/assets/homepageimg.webp" height={100} width={100} alt="homepage image" />
-      <h1>Welcome!</h1>
+    <div>
+    <Image className= {styles.landingimage} src="/../public/assets/homepageimg.webp" alt="homepage image" layout="fill" objectFit="cover" objectPosition="center"/> 
+      <div className={styles.homepage}>
+        <h1>Welcome!</h1>
       <h4>Take the stress out of solo travelling</h4>
       <button className="btn">Start Here</button>
+    </div>
     </div>
   )
 }
