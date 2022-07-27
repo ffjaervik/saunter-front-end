e0448cc02f5e2f2e82f23fdca7eb3ca552c5a451
Emily9903 <emily_9903@hotmail.com>
2022-07-27 15:00:40 +0100

    feat: image added

--- saunter/pages/get-started.js
+++ saunter/pages/get-started.js
@@ -1,13 +1,19 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
+import "../public/assets/inputpageimage.avif"
 
 
 const InputPage = () => {
   return (
-    <nav>
-      <h1>Start Here!</h1>
-    
-    </nav>
+    <div>
+    <Head>
+        <title>Saunter | Get-Started</title>
+    </Head>
+    <div>
+    <Image src="/../public/assets/inputpageimage.avif" alt="inputpage image" layout="responsive" width="10px" height="10px"/>  
+    </div>
+    </div>
   );
 }
  
--- saunter/pages/index.js
+++ saunter/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Homepage.module.css'
 import "../public/assets/homepageimg.webp"
@@ -6,16 +7,18 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div>
+    <Head>
+        <title>Saunter | Home</title>
+    </Head>
+    <div>
     <Image className= {styles.landingimage} src="/../public/assets/homepageimg.webp" alt="homepage image" layout="fill" objectFit="cover" objectPosition="center"/> 
       <div className={styles.homepage}>
         <h1>Welcome!</h1>
       <h4>Take the stress out of solo travelling</h4>
-    
-       {/* <button href="/get-started" className ="btn">Start Here</button> */}
        <Link href="/get-started">
-          <a className="btn"> Start Here</a>
-        </Link>
-    
+        <a className="btn">Start Here</a>
+      </Link>
+    </div>
     </div>
     </div>
   )
