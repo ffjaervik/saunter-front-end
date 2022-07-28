4fb8fa22f5512398f1c89bd13c5ac886a4e14648
Owen Ahmed <owenahmed@Owens-Mac-mini.local>
2022-07-28 11:41:38 +0100

    succesfully passed data between questionnaire and results pages. Collaborated with @anastasia-starostina and @ffjaervik

--- saunter/pages/questionnaire.js
+++ saunter/pages/questionnaire.js
@@ -1,3 +1,26 @@
-import Questionnaire from '../components/Questionnaire.js';
+import {useState, useEffect, useCallback} from "react";
+import axios from "axios";
+import { useRouter } from 'next/router';
 
+export default function CreateDayPlan() {
+  const router = useRouter()
+  function sendingResults(){
+    let location = "London"
+    let budget = "All"
+    router.push(
+      {
+        pathname: `/results`,
+        query: {location, budget}
+      }
+      )}
 
+  return (
+    <div>
+      <h1>Create Day Plan</h1>
+      <button type="button" onClick={sendingResults}>
+        Click here
+      </button>
+    </div>
+
+  );
+}
--- saunter/pages/results.js
+++ saunter/pages/results.js
@@ -1,5 +1,43 @@
 import {useRouter} from 'next/router';
+import { useState } from 'react'
 
-const router = useRouter();
-const data = router.query;
-console.log(data);
+
+// const data = router.query;
+// console.log(data);
+
+export default function Results(){
+    
+    const [submitting, setSubmitting] = useState(false);
+    const [data, setData] = useState([]);
+    const router = useRouter();
+    const {location, budget} = router.query
+
+//   useEffect(() => {
+//     const getData = async () => {
+//       const response = await axios.get(`http://localhost:3001/all-budgets`);
+//       setData(response.data.data);
+//       console.log(response.data.data)
+//       setSubmitting(false); // call is finished, set to false
+//     };
+
+    // query can change, but don't actually trigger
+    // request unless submitting is true
+
+//     if (submitting) {
+//       // is true initially, and again when button is clicked
+//       getData();
+//     }
+//   }, [submitting]);
+
+    return(
+        <div>
+            <p>{location}</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>{location}</p>
+            <p>{budget}</p>
+        </div>
+    )
+}
