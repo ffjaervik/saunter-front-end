ac1ee225b1d89df8008a6f1a872499673fe99a29
Emily9903 <emily_9903@hotmail.com>
2022-07-28 11:18:46 +0100

    feat: form and image added to inputpage, formatting in progress

--- saunter/pages/get-started.js
+++ saunter/pages/get-started.js
@@ -2,42 +2,49 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import "../public/assets/inputpageimage.avif";
-
-
+import { Box, ChakraProvider, FormControl, FormLabel, Select } from '@chakra-ui/react'
+import styles from "../styles/Inputpage.module.css"
 
 const InputPage = () => {
   return (
-   
-      <div>
-        <Head>
-          <title>Saunter | Get-Started</title>
-        </Head>
-        <div>
-          {/* <Image
-            src="/../public/assets/inputpageimage.avif"
-            alt="inputpage image"
-            layout="responsive"
-            width="10px"
-            height="10px"
-          /> */}
-
-          <FormControl>
+  <div>
+  <Head>
+    <title>Saunter | Get-Started</title>
+  </Head>
+ 
+  <div className={styles.form}>
+  <div className={styles.pagetwopic}>
+  <Image
+  src="/../public/assets/inputpageimage.avif"
+  alt="inputpage image"
+  layout="fixed"
+  objectFit="cover"
+  // display= "block"
+  width="100%"
+  height="100%"
+  />
+  </div>
+<ChakraProvider>
+<Box width="50vw">
+  <FormControl>
   <FormLabel>Country</FormLabel>
   <Select placeholder='Select Location'>
     <option>London</option>
-
   </Select>
+
   <FormLabel>Budget</FormLabel>
   <Select placeholder='Select Budget'>
     <option>High</option>
     <option>Medium</option>
     <option>Low</option>
-    
   </Select>
-</FormControl>
-        </div>
-      </div>
-      
+  
+  <button className="btn">Create Day Plan</button>
+  </FormControl>
+  </Box>
+  </ChakraProvider>
+  </div>
+  </div> 
   );
 };
 
