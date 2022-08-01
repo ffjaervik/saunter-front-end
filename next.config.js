a9834a1dffebfd2d2721d914ea78affd82dd26a4
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-08-01 15:18:45 +0100

    debug: frontend deployment

--- next.config.js
+++ next.config.js
@@ -16,11 +16,11 @@ const nextConfig = {
 module.exports = {
   images: {
     domains: [
-      'https://saunter-db.herokuapp.com',
-      'https://media.timeout.com/images/105162351/image.jpg',
-      'https://www.kew.org/sites/default/files/styles/highlights_module_main_image_770w_/public/2021-05/Wakehurst_early_summer_013_JH%20%282%29.jpg?itok=IGQKATBK',
-      'https://live.staticflickr.com/3841/14668921488_1f41631cd5_b.jpg',
-      'https://jennyshearawn.com/wp-content/uploads/2021/12/Oyster-Platter-with-Wild-Blueberry-Mignonette-Granita-15.jpg'
+      'saunter-db.herokuapp.com',
+      'media.timeout.com',
+      'kew.org',
+      'live.staticflickr.com',
+      'jennyshearawn.com'
     ], // hostname of the img url
   },
 };
