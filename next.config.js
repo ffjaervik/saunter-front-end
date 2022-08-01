c02225021d6f45b0219f1265928fad3df3041f06
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-08-01 15:00:11 +0100

    debug: frontend deployment

--- next.config.js
+++ next.config.js
@@ -16,7 +16,11 @@ const nextConfig = {
 module.exports = {
   images: {
     domains: [
-      'saunter-db.herokuapp.com'
+      'https://saunter-db.herokuapp.com',
+      'https://media.timeout.com/images/105162351/image.jpg',
+      'https://www.kew.org/sites/default/files/styles/highlights_module_main_image_770w_/public/2021-05/Wakehurst_early_summer_013_JH%20%282%29.jpg?itok=IGQKATBK',
+      'https://live.staticflickr.com/3841/14668921488_1f41631cd5_b.jpg',
+      'https://jennyshearawn.com/wp-content/uploads/2021/12/Oyster-Platter-with-Wild-Blueberry-Mignonette-Granita-15.jpg'
     ], // hostname of the img url
   },
 };
