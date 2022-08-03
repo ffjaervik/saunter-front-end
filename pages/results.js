edde159241e8257ad9bfa26a2a5e85c53970a66b
Emily9903 <emily_9903@hotmail.com>
2022-08-03 11:31:59 +0100

    feat: basic carousel added, collab with @mogoolmayab

--- pages/results.js
+++ pages/results.js
@@ -1,50 +1,57 @@
-import {useRouter} from 'next/router';
-import { useState, useEffect } from 'react'
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { Box, ChakraProvider, FormControl, FormLabel, Select } from '@chakra-ui/react'
-import styles from "../styles/Results.module.css"
-import Image from 'next/image'
-// import React, {useState} from 'https://cdn.skypack.dev/react';
-import ReactDOM from 'https://cdn.skypack.dev/react-dom';
-import {TiChevronLeftOutline, TiChevronRightOutline} from 'https://cdn.skypack.dev/react-icons/ti';
-
-
-
+import {
+  Box,
+  ChakraProvider,
+  FormControl,
+  FormLabel,
+  Select,
+} from "@chakra-ui/react";
+import styles from "../styles/Results.module.css";
+import Image from "next/image";
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { GiPadlockOpen } from 'react-icons/fa';
 
 // const data = router.query;
 // console.log(data);
 
-export default function Results(){
-    
-    const [data, setData] = useState([]);
-    const [budget, setBudget] = useState(null);
-    const router = useRouter();
-    const {selectedLocation, selectedBudget} = router.query;
+export default function Results() {
+  const [data, setData] = useState([]);
+  const [budget, setBudget] = useState(null);
+  const router = useRouter();
+  const { selectedLocation, selectedBudget } = router.query;
 
-//SAVE BUTTON FUNCTIONALITY
-    async function patchSaved(input) {
-      await fetch(`https://saunter-db.herokuapp.com/${router.query.selectedBudget}-budget`, {
+  //SAVE BUTTON FUNCTIONALITY
+  async function patchSaved(input) {
+    await fetch(
+      `https://saunter-db.herokuapp.com/${router.query.selectedBudget}-budget`,
+      {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Request-Method": "PATCH",
         },
         body: JSON.stringify(input),
-      });
-    }
+      }
+    );
+  }
 
-// UPDATE FORM DATA FUNCTIONALITY
+  // UPDATE FORM DATA FUNCTIONALITY
 
-    useEffect(() => {
+  useEffect(() => {
     const getData = async () => {
-      const response = await axios.get(`https://saunter-db.herokuapp.com/${router.query.selectedBudget}-budget`);
+      const response = await axios.get(
+        `https://saunter-db.herokuapp.com/${router.query.selectedBudget}-budget`
+      );
       setData(response.data.data);
-      console.log(response.data.data)
+      console.log(response.data.data);
     };
     // query can change, but don't actually trigger
     // request unless submitting is true
-      getData();
-
+    getData();
   }, [router.query.selectedBudget]);
 
   function sendingResults() {
@@ -57,67 +64,75 @@ export default function Results(){
   }
 
 
-
-    return (
-      <div className={styles.main}>
-        <h1>Your Recommendations:</h1>
-        <div className={styles.results}>
+  return (
+    <div className={styles.main}>
+      <h1>Your Recommendations:</h1>
+      <div className={styles.results}>
+        <Carousel className={styles.carousel}
+        infiniteLoop
+        centerMode
+        centerSlidePercentage="70"
+        
+        >
           {data.map((activity) => {
             console.log(activity.name);
             const id = activity.id;
             const name = activity.name;
             const image = activity.image;
-            const body = {id: id}
+            const body = { id: id };
             return (
-
               <div className={styles.activity} key={name}>
                 <h5>{name}</h5>
                 <div className={styles.imagebtn}>
-                <img src={image} alt="/" />
-                <button onClick={function(){return patchSaved(body)}} key={id} className="btn">Save</button>
+                  <img src={image} alt="/" />
+                  <button
+                    onClick={function () {
+                      return patchSaved(body);
+                    }}
+                    key={id}
+                    className="btn"
+                  >
+                    Save
+                  </button>
                 </div>
               </div>
             );
           })}
-        </div>
-        {/* chakra ui imported below */}
-        <div className="form">
-          <ChakraProvider>
-            <Box
-              width="15vw"
-              padding="6"
-              borderRadius="2rem"
-              m = "15vh"
-            >
-              <FormControl>
-                <FormLabel>Location</FormLabel>
-                <Select placeholder="Select Location">
-                  <option>London</option>
-                </Select>
+        </Carousel>
+      </div>
+      {/* chakra ui imported below */}
+      <div className="form">
+        <ChakraProvider>
+          <Box width="15vw" padding="6" borderRadius="2rem" m="15vh">
+            <FormControl>
+              <FormLabel>Location</FormLabel>
+              <Select placeholder="Select Location">
+                <option>London</option>
+              </Select>
 
+              <FormLabel>Budget</FormLabel>
 
-                <FormLabel>Budget</FormLabel>
+              <Select
+                placeholder="Select Budget"
+                value={budget}
+                onChange={(e) => setBudget(e.target.value)}
+              >
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </Select>
 
-                <Select
-                  placeholder="Select Budget"
-                  value={budget}
-                  onChange={(e) => setBudget(e.target.value)}
-                >
-                  <option value="low">Low</option>
-                  <option value="medium">Medium</option>
-                  <option value="high">High</option>
-                </Select>
-
-                <div className={styles.daybtn}>
+              <div className={styles.daybtn}>
                 <button className="btn" onClick={sendingResults}>
                   Create Day Plan
                 </button>
-                </div>
-
-              </FormControl>
-            </Box>
-          </ChakraProvider>
-        </div>
+              </div>
+              {/* <GiPadlockOpen/> */}
+            </FormControl>
+          </Box>
+        </ChakraProvider>
+        
       </div>
-    );
+    </div>
+  );
 }
