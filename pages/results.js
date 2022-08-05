e3f8cb53af8e90f0e86f0b9e82206454e815d62e
Owen Ahmed <owenahmed@Owens-Mac-mini.local>
2022-08-05 13:54:14 +0100

    Feat: added carousel button. Collaborated with @anastasia-starostina.

--- pages/results.js
+++ pages/results.js
@@ -5,76 +5,7 @@ import { Box, ChakraProvider, FormControl, FormLabel, Select } from '@chakra-ui/
 import styles from "../styles/Results.module.css"
 import Image from 'next/image'
 
-export default function Results(){
-    
-    const [data, setData] = useState([]);
-    const [budget, setBudget] = useState(null);
-    const [energy, setEnergy] = useState(null);
-    const [dog, setDog] = useState(null)
-    const [update, setUpdate] = useState(0)
-    const [list, setList] = useState([]);
-    const router = useRouter();
-    const {selectedLocation, selectedBudget, selectedEnergy, selectedDog} = router.query;
-
-//SAVE BUTTON FUNCTIONALITY
-    async function patchSaved(input) {
-      await fetch(`https://saunter-db.herokuapp.com/${router.query.selectedBudget}-budget`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Request-Method": "PATCH",
-        },
-        body: JSON.stringify(input),
-      });
-    }
-
-// UPDATE FORM DATA FUNCTIONALITY
-
-    useEffect(() => {
-    const getData = async () => {
-      const response = await axios.get(`https://saunter-db.herokuapp.com/all-budgets`);
-      let allActivities = response.data.data;
-      let filteredActivities = [];
-
-      if(router.query.selectedDog === 'true'){
-        router.query.selectedDog = true
-      } else if(router.query.selectedDog === 'false'){
-        router.query.selectedDog = false
-      }
-      console.log(`Router: ${router.query.selectedDog}`)
-      console.log(`RouterTO: ${typeof router.query.selectedDog}`)
-      
-      for(let i = 0; i < allActivities.length; i++){
-        if(allActivities[i].budget == router.query.selectedBudget && allActivities[i].energy_level == router.query.selectedEnergy && allActivities[i].dog_friendly == router.query.selectedDog){
-          console.log(`Activity: ${allActivities[i].dog_friendly}`)
-          console.log(`ActivityTO: ${typeof allActivities[i].dog_friendly}`)
-          filteredActivities.push(allActivities[i])
-        }
-      }
-      setData(filteredActivities)
-      console.log(filteredActivities)
-    };
-    // query can change, but don't actually trigger
-    // request unless submitting is true
-    getData();
-    console.log(`Update: ${update}`)
-
-
-  }, [update]);
-
-  function sendingResults() {
-    router.query.selectedBudget = budget;
-    router.query.selectedEnergy = energy;
-    router.query.selectedDog = dog;
-    console.log(`Budget: ${budget}`);
-    console.log(`Energy: ${energy}`);
-    console.log(`Dog: ${dog}`)
-    setUpdate(update + 1)
-  }
-
-
 //CAROUSEL START
-const CARDS = 10;
 const MAX_VISIBILITY = 3;
 
 const Card = ({title, content}) => (
@@ -87,7 +18,7 @@ const Card = ({title, content}) => (
 
 const Carousel = ({children}) => {
     const [active, setActive] = useState(0);
-    const count = data.length;
+    const count = 10;
     
     return (
       <div className={styles.carousel}>
@@ -112,6 +43,125 @@ const Carousel = ({children}) => {
         </button>}
       </div>
     )};
+
+export default function Results(){
+    
+    const [data, setData] = useState([]);
+    const [budget, setBudget] = useState(null);
+    const [energy, setEnergy] = useState(null);
+    const [dog, setDog] = useState(null)
+    const [update, setUpdate] = useState(0)
+    const [list, setList] = useState([]);
+    const router = useRouter();
+    const {selectedLocation, selectedBudget, selectedEnergy, selectedDog} = router.query;
+    console.log(`List:`, list)
+//SAVE BUTTON FUNCTIONALITY
+    async function patchSaved(input) {
+      await fetch(`https://saunter-db.herokuapp.com/${router.query.selectedBudget}-budget`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Request-Method": "PATCH",
+        },
+        body: JSON.stringify(input),
+      });
+    }
+
+// UPDATE FORM DATA FUNCTIONALITY
+
+    useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(`https://saunter-db.herokuapp.com/all-budgets`);
+      let allActivities = response.data.data;
+      console.log('allActivities', allActivities)
+      let filteredActivities = [];
+
+      if(router.query.selectedDog === 'true'){
+        router.query.selectedDog = true
+      } else if(router.query.selectedDog === 'false'){
+        router.query.selectedDog = false
+      }
+      console.log(`Router: ${router.query.selectedDog}`)
+      console.log(`RouterTO: ${typeof router.query.selectedDog}`)
+      
+      for(let i = 0; i < allActivities.length; i++){
+        if(allActivities[i].budget == router.query.selectedBudget && allActivities[i].energy_level == router.query.selectedEnergy && allActivities[i].dog_friendly == router.query.selectedDog){
+          console.log(`Activity: ${allActivities[i].dog_friendly}`)
+          console.log(`ActivityTO: ${typeof allActivities[i].dog_friendly}`)
+          filteredActivities.push(allActivities[i])
+        }
+      }
+      setData(filteredActivities)
+      console.log('filtered', filteredActivities)
+
+      if(update === 0){
+        console.log('Setting list')
+        setList([filteredActivities])
+      }
+    };
+    [{}]
+    // query can change, but don't actually trigger
+    // request unless submitting is true
+    getData();
+    console.log(`Update: ${update}`)
+    console.log(`List: ${list}`)
+
+
+  }, [update]);
+
+  function sendingResults() {
+    router.query.selectedBudget = budget;
+    router.query.selectedEnergy = energy;
+    router.query.selectedDog = dog;
+    console.log(`Budget: ${budget}`);
+    console.log(`Energy: ${energy}`);
+    console.log(`Dog: ${dog}`)
+    setUpdate(update + 1)
+  }
+
+  function addCarousel(){
+    setList([...list, data]);
+    // setUpdate(update + 1)
+  }
+
+//CAROUSEL START
+// const MAX_VISIBILITY = 3;
+
+// const Card = ({title, content}) => (
+//   <div className={styles.card}>
+//     <h2>{title}</h2>
+//     <p>{content}</p>
+//   </div>
+// );
+
+
+// const Carousel = ({children}) => {
+//     const [active, setActive] = useState(0);
+//     const count = data.length;
+    
+//     return (
+//       <div className={styles.carousel}>
+//         {active > 0 && <button className={styles.navleft} onClick={function(){setActive(active - 1)}}>
+//                          ==
+//                        </button>}
+//         {Children.map(children, (child, i) => (
+//           <div className={styles.cardcontainer} style={{
+//               '--active': i === active ? 1 : 0,
+//               '--offset': (active - i) / 3,
+//               '--direction': Math.sign(active - i),
+//               '--abs-offset': Math.abs(active - i) / 3,
+//               'pointer-events': active === i ? 'auto' : 'none',
+//               'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
+//               'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
+//             }}>
+//             {child}
+//           </div>
+//         ))}
+//         {active < count - 1 && <button className={styles.navright} onClick={function(){setActive(active + 1)}}>
+//         ==
+//         </button>}
+//       </div>
+//     )};
 
 //CAROUSEL END
 
@@ -142,6 +192,7 @@ const Carousel = ({children}) => {
   //1. Carousels 
   //2. Data in the 🎠
   // Save data as state
+  // Ensure list is an array of arrays: [[{}]]
 
 
     return (
@@ -150,14 +201,15 @@ const Carousel = ({children}) => {
         <div className={styles.results}>
         </div>
         <div className={styles.app}>
-           <Carousel>
-             {data.map((activity, index) => (
+        {list.map((carousel, index) => (
+           <Carousel key={index}>
+             {carousel.map((activity, index) => (
                <Card key={index} title={activity.name} content={activity.description}/>
              ))}
            </Carousel>
-
-           <button>Click here</button>
-
+        ))
+        }
+           <button className={styles.addCarousel} onClick={addCarousel}>Click here</button>
         </div>
         
         {/* chakra ui imported below */}
