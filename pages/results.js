95380a4fc07a14ea44dd31497b842dd97c1aa5ae
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-08-05 14:40:59 +0100

    style: adding button to add new activity and changing border radius of images in carouse. Colab with @owenowenson

--- pages/results.js
+++ pages/results.js
@@ -1,112 +1,147 @@
-import {useRouter} from 'next/router';
-import { useState, useEffect, Children } from 'react'
+import { useRouter } from "next/router";
+import { useState, useEffect, Children } from "react";
 import axios from "axios";
-import { Box, ChakraProvider, FormControl, FormLabel, Select } from '@chakra-ui/react'
-import styles from "../styles/Results.module.css"
-import Image from 'next/image'
+import {
+  Box,
+  ChakraProvider,
+  FormControl,
+  FormLabel,
+  Select,
+} from "@chakra-ui/react";
+import styles from "../styles/Results.module.css";
+import Image from "next/image";
+import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { IconContext } from "react-icons";
 
 //CAROUSEL START
 const MAX_VISIBILITY = 3;
 
-const Card = ({title, content}) => (
+const Card = ({ title, content, image }) => (
   <div className={styles.card}>
     <h2>{title}</h2>
     <p>{content}</p>
+    <img src={image} alt="Activity Card Image" className={styles.card_image} />
   </div>
 );
 
+const Carousel = ({ children }) => {
+  const [active, setActive] = useState(0);
+  const count = 10;
 
-const Carousel = ({children}) => {
-    const [active, setActive] = useState(0);
-    const count = 10;
-    
-    return (
-      <div className={styles.carousel}>
-        {active > 0 && <button className={styles.navleft} onClick={function(){setActive(active - 1)}}>
-                         ==
-                       </button>}
-        {Children.map(children, (child, i) => (
-          <div className={styles.cardcontainer} style={{
-              '--active': i === active ? 1 : 0,
-              '--offset': (active - i) / 3,
-              '--direction': Math.sign(active - i),
-              '--abs-offset': Math.abs(active - i) / 3,
-              'pointer-events': active === i ? 'auto' : 'none',
-              'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
-              'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
-            }}>
-            {child}
-          </div>
-        ))}
-        {active < count - 1 && <button className={styles.navright} onClick={function(){setActive(active + 1)}}>
-        ==
-        </button>}
-      </div>
-    )};
+  return (
+    <div className={styles.carousel}>
+      {active > 0 && (
+        <button
+          className={styles.navleft}
+          onClick={function () {
+            setActive(active - 1);
+          }}
+        >
+          <FaChevronCircleLeft />
+        </button>
+      )}
+      {Children.map(children, (child, i) => (
+        <div
+          className={styles.cardcontainer}
+          style={{
+            "--active": i === active ? 1 : 0,
+            "--offset": (active - i) / 3,
+            "--direction": Math.sign(active - i),
+            "--abs-offset": Math.abs(active - i) / 3,
+            "pointer-events": active === i ? "auto" : "none",
+            opacity: Math.abs(active - i) >= MAX_VISIBILITY ? "0" : "1",
+            display: Math.abs(active - i) > MAX_VISIBILITY ? "none" : "block",
+          }}
+        >
+          {child}
+        </div>
+      ))}
+      {active < count - 1 && (
+        <button
+          className={styles.navright}
+          onClick={function () {
+            setActive(active + 1);
+          }}
+        >
+          <FaChevronCircleRight />
+        </button>
+      )}
+    </div>
+  );
+};
 
-export default function Results(){
-    
-    const [data, setData] = useState([]);
-    const [budget, setBudget] = useState(null);
-    const [energy, setEnergy] = useState(null);
-    const [dog, setDog] = useState(null)
-    const [update, setUpdate] = useState(0)
-    const [list, setList] = useState([]);
-    const router = useRouter();
-    const {selectedLocation, selectedBudget, selectedEnergy, selectedDog} = router.query;
-    console.log(`List:`, list)
-//SAVE BUTTON FUNCTIONALITY
-    async function patchSaved(input) {
-      await fetch(`https://saunter-db.herokuapp.com/${router.query.selectedBudget}-budget`, {
+export default function Results() {
+  const [data, setData] = useState([]);
+  const [budget, setBudget] = useState(null);
+  const [energy, setEnergy] = useState(null);
+  const [dog, setDog] = useState(null);
+  const [update, setUpdate] = useState(0);
+  const [list, setList] = useState([]);
+  const router = useRouter();
+  const { selectedLocation, selectedBudget, selectedEnergy, selectedDog } =
+    router.query;
+  console.log(`List:`, list);
+  //SAVE BUTTON FUNCTIONALITY
+  async function patchSaved(input) {
+    await fetch(
+      `https://saunter-db.herokuapp.com/${router.query.selectedBudget}-budget`,
+      {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Request-Method": "PATCH",
         },
         body: JSON.stringify(input),
-      });
-    }
+      }
+    );
+  }
 
-// UPDATE FORM DATA FUNCTIONALITY
+  // UPDATE FORM DATA FUNCTIONALITY
 
-    useEffect(() => {
+  useEffect(() => {
     const getData = async () => {
-      const response = await axios.get(`https://saunter-db.herokuapp.com/all-budgets`);
+      const response = await axios.get(
+        `https://saunter-db.herokuapp.com/all-budgets`
+      );
       let allActivities = response.data.data;
-      console.log('allActivities', allActivities)
+      console.log("allActivities", allActivities);
       let filteredActivities = [];
 
-      if(router.query.selectedDog === 'true'){
-        router.query.selectedDog = true
-      } else if(router.query.selectedDog === 'false'){
-        router.query.selectedDog = false
+      if (router.query.selectedDog === "true") {
+        router.query.selectedDog = true;
+      } else if (router.query.selectedDog === "false") {
+        router.query.selectedDog = false;
       }
-      console.log(`Router: ${router.query.selectedDog}`)
-      console.log(`RouterTO: ${typeof router.query.selectedDog}`)
-      
-      for(let i = 0; i < allActivities.length; i++){
-        if(allActivities[i].budget == router.query.selectedBudget && allActivities[i].energy_level == router.query.selectedEnergy && allActivities[i].dog_friendly == router.query.selectedDog){
-          console.log(`Activity: ${allActivities[i].dog_friendly}`)
-          console.log(`ActivityTO: ${typeof allActivities[i].dog_friendly}`)
-          filteredActivities.push(allActivities[i])
+      console.log(`Router: ${router.query.selectedDog}`);
+      console.log(`RouterTO: ${typeof router.query.selectedDog}`);
+
+      for (let i = 0; i < allActivities.length; i++) {
+        if (
+          allActivities[i].budget == router.query.selectedBudget &&
+          allActivities[i].energy_level == router.query.selectedEnergy &&
+          allActivities[i].dog_friendly == router.query.selectedDog
+        ) {
+          console.log(`Activity: ${allActivities[i].dog_friendly}`);
+          console.log(`ActivityTO: ${typeof allActivities[i].dog_friendly}`);
+          filteredActivities.push(allActivities[i]);
         }
       }
-      setData(filteredActivities)
-      console.log('filtered', filteredActivities)
+      setData(filteredActivities);
+      console.log("filtered", filteredActivities);
 
-      if(update === 0){
-        console.log('Setting list')
-        setList([filteredActivities])
+      if (update === 0) {
+        console.log("Setting list");
+        setList([filteredActivities]);
       }
     };
-    [{}]
+    [{}];
     // query can change, but don't actually trigger
     // request unless submitting is true
     getData();
-    console.log(`Update: ${update}`)
-    console.log(`List: ${list}`)
-
-
+    console.log(`Update: ${update}`);
+    console.log(`List: ${list}`);
   }, [update]);
 
   function sendingResults() {
@@ -115,55 +150,54 @@ export default function Results(){
     router.query.selectedDog = dog;
     console.log(`Budget: ${budget}`);
     console.log(`Energy: ${energy}`);
-    console.log(`Dog: ${dog}`)
-    setUpdate(update + 1)
+    console.log(`Dog: ${dog}`);
+    setUpdate(update + 1);
   }
 
-  function addCarousel(){
+  function addCarousel() {
     setList([...list, data]);
     // setUpdate(update + 1)
   }
 
-//CAROUSEL START
-// const MAX_VISIBILITY = 3;
+  //CAROUSEL START
+  // const MAX_VISIBILITY = 3;
 
-// const Card = ({title, content}) => (
-//   <div className={styles.card}>
-//     <h2>{title}</h2>
-//     <p>{content}</p>
-//   </div>
-// );
+  // const Card = ({title, content}) => (
+  //   <div className={styles.card}>
+  //     <h2>{title}</h2>
+  //     <p>{content}</p>
+  //   </div>
+  // );
 
+  // const Carousel = ({children}) => {
+  //     const [active, setActive] = useState(0);
+  //     const count = data.length;
 
-// const Carousel = ({children}) => {
-//     const [active, setActive] = useState(0);
-//     const count = data.length;
-    
-//     return (
-//       <div className={styles.carousel}>
-//         {active > 0 && <button className={styles.navleft} onClick={function(){setActive(active - 1)}}>
-//                          ==
-//                        </button>}
-//         {Children.map(children, (child, i) => (
-//           <div className={styles.cardcontainer} style={{
-//               '--active': i === active ? 1 : 0,
-//               '--offset': (active - i) / 3,
-//               '--direction': Math.sign(active - i),
-//               '--abs-offset': Math.abs(active - i) / 3,
-//               'pointer-events': active === i ? 'auto' : 'none',
-//               'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
-//               'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
-//             }}>
-//             {child}
-//           </div>
-//         ))}
-//         {active < count - 1 && <button className={styles.navright} onClick={function(){setActive(active + 1)}}>
-//         ==
-//         </button>}
-//       </div>
-//     )};
+  //     return (
+  //       <div className={styles.carousel}>
+  //         {active > 0 && <button className={styles.navleft} onClick={function(){setActive(active - 1)}}>
+  //                          ==
+  //                        </button>}
+  //         {Children.map(children, (child, i) => (
+  //           <div className={styles.cardcontainer} style={{
+  //               '--active': i === active ? 1 : 0,
+  //               '--offset': (active - i) / 3,
+  //               '--direction': Math.sign(active - i),
+  //               '--abs-offset': Math.abs(active - i) / 3,
+  //               'pointer-events': active === i ? 'auto' : 'none',
+  //               'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
+  //               'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
+  //             }}>
+  //             {child}
+  //           </div>
+  //         ))}
+  //         {active < count - 1 && <button className={styles.navright} onClick={function(){setActive(active + 1)}}>
+  //         ==
+  //         </button>}
+  //       </div>
+  //     )};
 
-//CAROUSEL END
+  //CAROUSEL END
 
   //div className={styles.results}
 
@@ -184,92 +218,88 @@ export default function Results(){
   //   );
   // })}
 
-
   //Create a state for the list of carousels
   //Each time 'Add new activity' button is clickes, add new carousel to the list with spread operator
   // State is [list, setList]
   //Map twice:
-  //1. Carousels 
+  //1. Carousels
   //2. Data in the 🎠
   // Save data as state
   // Ensure list is an array of arrays: [[{}]]
+  //for description content={activity.description}
 
-
-    return (
-      <div className={styles.main}>
-        <h1>Your Recommendations:</h1>
-        <div className={styles.results}>
-        </div>
-        <div className={styles.app}>
+  return (
+    <div className={styles.main}>
+      <h1>Your Recommendations:</h1>
+      <div className={styles.results}></div>
+      <div className={styles.app}>
         {list.map((carousel, index) => (
-           <Carousel key={index}>
-             {carousel.map((activity, index) => (
-               <Card key={index} title={activity.name} content={activity.description}/>
-             ))}
-           </Carousel>
-        ))
-        }
-           <button className={styles.addCarousel} onClick={addCarousel}>Click here</button>
-        </div>
-        
-        {/* chakra ui imported below */}
-        <div className="form">
-          <ChakraProvider>
-            <Box
-              width="15vw"
-              padding="6"
-              borderRadius="2rem"
-              m = "15vh"
-            >
-              <FormControl>
-                <FormLabel>Location</FormLabel>
-                <Select placeholder="Select location">
-                  <option>London</option>
-                </Select>
+          <Carousel key={index}>
+            {carousel.map((activity, index) => (
+              <Card key={index} title={activity.name} image={activity.image} />
+            ))}
+          </Carousel>
+        ))}
+        <button className={styles.addCarousel} onClick={addCarousel}>
+          <IconContext.Provider
+            value={{ color: "black", className: "global-class-name", size: "3rem" }}
+          >
+            <AiOutlinePlusCircle />
+          </IconContext.Provider>
+        </button>
+      </div>
 
+      {/* chakra ui imported below */}
+      <div className="form">
+        <ChakraProvider>
+          <Box width="15vw" padding="6" borderRadius="2rem" m="15vh">
+            <FormControl>
+              <FormLabel>Location</FormLabel>
+              <Select placeholder="Select location">
+                <option>London</option>
+              </Select>
 
-                <FormLabel>Budget</FormLabel>
-                <Select
-                  placeholder="Select budget"
-                  value={budget}
-                  onChange={(e) => setBudget(e.target.value)}
-                >
-                  <option value="1">Low</option>
-                  <option value="2">Medium</option>
-                  <option value="3">High</option>
-                </Select>
+              <FormLabel>Budget</FormLabel>
+              <Select
+                placeholder="Select budget"
+                value={budget}
+                onChange={(e) => setBudget(e.target.value)}
+              >
+                <option value="1">Low</option>
+                <option value="2">Medium</option>
+                <option value="3">High</option>
+              </Select>
 
-                <FormLabel>Energy level</FormLabel>
-                <Select 
-                  placeholder='Select energy level' 
-                  value={energy} 
-                  onChange={(e) => setEnergy(e.target.value)}
-                >
-                  <option value="1">Low</option>
-                  <option value="2">Medium</option>
-                  <option value="3">High</option>
-                </Select>
-                
-                <FormLabel>Dog friendly</FormLabel>
-                <Select 
-                  placeholder='Select preference' 
-                  value={dog} 
-                  onChange={(e) => setDog(e.target.value)}
-                >
-                  <option value="true">Yes</option>
-                  <option value="false">No</option>
-                </Select>
+              <FormLabel>Energy level</FormLabel>
+              <Select
+                placeholder="Select energy level"
+                value={energy}
+                onChange={(e) => setEnergy(e.target.value)}
+              >
+                <option value="1">Low</option>
+                <option value="2">Medium</option>
+                <option value="3">High</option>
+              </Select>
 
-                <div className={styles.daybtn}>
+              <FormLabel>Dog friendly</FormLabel>
+              <Select
+                placeholder="Select preference"
+                value={dog}
+                onChange={(e) => setDog(e.target.value)}
+              >
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+              </Select>
+
+              <div className={styles.daybtn}>
                 <button className="btn" onClick={sendingResults}>
                   Create Day Plan
                 </button>
-                </div>
-
-              </FormControl>
-            </Box>
-          </ChakraProvider>
-        </div>
+              </div>
+            </FormControl>
+          </Box>
+        </ChakraProvider>
       </div>
-    );
+    </div>
+  );
 }
