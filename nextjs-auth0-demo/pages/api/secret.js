be03dde50cb1bf89f0dafc2a918a9e2c11efbee3
May <MogoolMayaB@users.noreply.github.com>
2022-08-08 12:31:37 +0100

    feat: auth0 setup with login and logout collab with Anastasia

---
+++ nextjs-auth0-demo/pages/api/secret.js
@@ -0,0 +1,8 @@
+import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0'
+
+export default withApiAuthRequired(function SecretRoute(reg, res) {
+    const session = getSession(req, res)
+    const user = session.user 
+
+//...rest of the code will go here 
+})
---
+++ nextjs-auth0-demo/pages/membersonly/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const members = () => {
+  return (
+    <h1>Your Day Plan!</h1>
+  )
+}
+
+export default members
