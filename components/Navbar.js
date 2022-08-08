96281200e257c9f7194b0034e3e675f8065df4f3
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-08-08 17:41:57 +0100

    feat: Auth0 in navbar + conditional rendering. Collaboration: @anastasia-starostina

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,73 +1,97 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from "../styles/Navbar.module.css"
-import { useUser } from "@auth0/nextjs-auth0";
-import PropTypes from "prop-types";
+import styles from '../styles/Navbar.module.css'
+import { useUser } from '@auth0/nextjs-auth0'
+import PropTypes from 'prop-types'
 
-const Navbar = ({ isLoggedIn }) => {
-  const { user } = useUser();
+const Navbar = () => {
+	const { user, error, isLoading } = useUser()
+	console.log(user)
 
+	return (
+		<nav className={styles.navbar}>
+			<Link href='/'>
+				<h2>Saunter</h2>
+			</Link>
+			<Link href='/'>
+				<a>Home</a>
+			</Link>
+			<Link href='/about'>
+				<a>About</a>
+			</Link>
+			<Link href='/community/'>
+				<a>Community</a>
+			</Link>
+			<Link href='/contact/'>
+				<a>Contact</a>
+			</Link>
+			{error && <div>{error.message}</div>}
+			{user && (
+				<div className='loginwelcome'>
+					<button>
+						{' '}
+						Welcome {user.name}! <a href='/api/auth/logout'>Logout</a>{' '}
+					</button>
+				</div>
+			)}
+			{!user && (
+				<a className='loginbutton' href='/api/auth/login'>
+					Login
+				</a>
+			)}
+		</nav>
+	)
+}
 
-    return (
-      <nav className={styles.navbar}>
-        <Link href="/">
-          <h2>Saunter</h2>
-        </Link>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        <Link href="/community/">
-          <a>Community</a>
-        </Link>
-        <Link href="/contact/">
-          <a>Contact</a>
-        </Link>
-        <button className="btn" href="/api/auth/logout">
-          Logout
-        </button>
-      </nav>
-    );
-  }
+//   const { user, error, isLoading } = useUser();
 
+//   if (isLoading) return <div>Loading...</div>;
+//   if (error) return <div>{error.message}</div>;
 
-  
-  //{
-    /* <div className="loginwelcome">
+//   if (user) {
+//     return (
+//       <div className="loginwelcome">
+//         Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+//       </div>
+//     );
+//   }
+
+//   return <a className="loginbutton" href="/api/auth/login">Login</a>;
+// }
+
+//{
+/* <div className="loginwelcome">
         Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
       </div> */
-  //}
+//}
 
-  // return (
-  //   <a className="loginbutton" href="/api/auth/login">
-  //     Login
-  //   </a>
-  // );
+// return (
+//   <a className="loginbutton" href="/api/auth/login">
+//     Login
+//   </a>
+// );
 
-  // return (
-  //   <nav className={styles.navbar}>
-  //     <Link href="/">
-  //       <h2>Saunter</h2>
-  //     </Link>
-  //     <Link href="/">
-  //       <a>Home</a>
-  //     </Link>
-  //     <Link href="/about">
-  //       <a>About</a>
-  //     </Link>
-  //     <Link href="/community/">
-  //       <a>Community</a>
-  //     </Link>
-  //     <Link href="/contact/">
-  //       <a>Contact</a>
-  //     </Link>
-  //     <button className="btn" href="/api/auth/login">
-  //       Login
-  //     </button>
-  //   </nav>
-  // );
+// return (
+//   <nav className={styles.navbar}>
+//     <Link href="/">
+//       <h2>Saunter</h2>
+//     </Link>
+//     <Link href="/">
+//       <a>Home</a>
+//     </Link>
+//     <Link href="/about">
+//       <a>About</a>
+//     </Link>
+//     <Link href="/community/">
+//       <a>Community</a>
+//     </Link>
+//     <Link href="/contact/">
+//       <a>Contact</a>
+//     </Link>
+//     <button className="btn" href="/api/auth/login">
+//       Login
+//     </button>
+//   </nav>
+// );
 
- 
-export default Navbar;
+export default Navbar
