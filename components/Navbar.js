77e6962f49c486eab458669a11f7eb178f39cbc3
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-08-08 22:15:59 +0100

    style: changed login button so that it fits on the navbar and adobts the style of styles.navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,38 +9,34 @@ const Navbar = () => {
 	console.log(user)
 
 	return (
-		<nav className={styles.navbar}>
-			<Link href='/'>
-				<h2>Saunter</h2>
-			</Link>
-			<Link href='/'>
-				<a>Home</a>
-			</Link>
-			<Link href='/about'>
-				<a>About</a>
-			</Link>
-			<Link href='/community/'>
-				<a>Community</a>
-			</Link>
-			<Link href='/contact/'>
-				<a>Contact</a>
-			</Link>
-			{error && <div>{error.message}</div>}
-			{user && (
-				<div >
-					<button>
-						{' '}
-						Welcome {user.name}! <Link href='/api/auth/logout'>Logout</Link>{' '}
-					</button>
-				</div>
-			)}
-			{!user && (
-				<Link href='/api/auth/login'>
-					Login
-				</Link>
-			)}
-		</nav>
-	)
+    <nav className={styles.navbar}>
+      <Link href="/">
+        <h2>Saunter</h2>
+      </Link>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/community/">
+        <a>Community</a>
+      </Link>
+      <Link href="/contact/">
+        <a>Contact</a>
+      </Link>
+      {error && <div>{error.message}</div>}
+      {user && (
+        <div>
+          <button >
+            {" "}
+            Welcome {user.name}! <Link href="/api/auth/logout">Logout</Link>{" "}
+          </button>
+        </div>
+      )}
+      {!user && <Link href="/api/auth/login">Login</Link>}
+    </nav>
+  );
 }
 
 //   const { user, error, isLoading } = useUser();
