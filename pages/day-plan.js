f958e2fa81c2bb672ff68674f98b786a2022abf4
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-08-09 11:46:12 +0100

    fetch request and filter on day-plan.js. Collaborated @owenowensen

--- pages/day-plan.js
+++ pages/day-plan.js
@@ -1,6 +1,35 @@
 import styles from '../styles/dayplan.module.css'
+import {useEffect} from 'react'
+import {useRouter} from "next/router"
+import axios from 'axios'
 
 export default function GetStarted() {
+
+	  const router = useRouter()
+	  const {cart} = router.query;
+	  console.log (`This is your cart:`, cart)
+
+	  useEffect(() => {
+		const getData = async () => {
+			const response = await axios.get(
+				`https://saunter-db.herokuapp.com/all-budgets`
+			)
+			let allActivities = response.data.data
+			let filteredActivities = []
+			for (let i = 0; i < cart.length; i++){
+				let activityId = cart[i]
+				for (let i = 0; i < allActivities.length; i++) {
+					if (allActivities[i].id == activityId) {
+						filteredActivities.push(allActivities[i])
+					}
+				}
+			}
+			console.log(filteredActivities)
+			}
+		getData()
+		},[])
+	
+
 	return (
 		<div className={styles.dayplan_container}>
 			<div className={styles.dayplan}>
