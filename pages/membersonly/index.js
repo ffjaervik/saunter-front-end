025adefe45170174614093099da551ced466d71e
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-08-11 17:42:05 +0100

    WIP user-page

--- pages/membersonly/index.js
+++ pages/membersonly/index.js
@@ -1,8 +1,73 @@
 import React from 'react'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import styles from '../../styles/User.module.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { FiMapPin } from 'react-icons/fi'
+import { IconContext } from 'react-icons'
 
-const members = () => {
+const Members = () => {
+	const [savedDayplans, setSavedDayplans] = useState([])
+	const [savedActivities, setSavedActivities] = useState([])
+
+	// useEffect(() => {
+	// 	const getData = async () => {
+	// 		const response = await axios.get(
+	// 			`https://saunter-db.herokuapp.com/dayplans`
+	// 		)
+	// 		let dayplans = response.data.data
+	// 		console.log("dayplan: ", dayplans)
+	// 		setSavedDayplans(dayplans[0].name)
+	//     console.log("savedDayplans: ", savedDayplans)
+	// 	}
+	//   getData()
+	// }, [])
+
+	//FETCH ALL SAVED ACTIVITIES FROM DB without axios
+	// useEffect(() => {
+	//   const getData = async () => {
+	//     const response = await res.json()
+	//     let data = response.data.data
+	//     console.log("dayplan: ", data)
+	//     setLikedActivities(data)
+	//   }
+	//   getData()
+	// } , [])
+
+	useEffect(() => {
+		const getData = async () => {
+			const response = await axios.get('https://saunter-db.herokuapp.com/saved')
+			let data = response.data.data
+			setSavedActivities(data)
+			console.log('savedActivities:', data)
+		}
+		getData()
+	}, [])
+
+	const LikedCard = ({ name, type, description, image }) => {
+		return (
+			<div className={styles.liked_container}>
+				<div className={styles.image_container}>
+					<img src={image} alt={name} />
+				</div>
+				<h2>{name}</h2>
+				{/* On hover should display description */}
+				<div className={styles.hover}>
+					<h5>{type}</h5>
+					<p>{description}</p>
+					<div>
+						<div>
+							<IconContext.Provider value={{ color: 'black', size: '2rem' }}>
+								<FiMapPin />
+							</IconContext.Provider>
+						</div>
+					</div>
+					<h2>{name}</h2>
+				</div>
+			</div>
+		)
+	}
+
 	return (
 		<div className={styles.main}>
 			<div className={styles.dayplan_title}>
@@ -15,18 +80,27 @@ const members = () => {
 				</div>
 			</div>
 			<div className={styles.liked_title}>
-				<h1>Your liked activities:</h1>
+				<h1>Your liked activities: </h1>
 			</div>
 			<div className={styles.liked_container_main}>
 				{/* Map though liked activities here */}
-				<div className={styles.liked_container}>
-        {/* Mapping ends here */}
-        </div>/
+				
+					{savedActivities.map((activity, index) => (
+						<LikedCard
+							key={index}
+							name={activity.name}
+							type={activity.type}
+							description={activity.description}
+							image={activity.image}
+						/>
+					))}
+			
+					{/* Mapping ends here */}
 			</div>
 		</div>
 	)
 }
 
-export default members
+export default Members
 
 export const getServerSideProps = withPageAuthRequired()
