5ed6bb2135fec1bad7ebd94be77d38e10ddb503c
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-08-11 10:55:54 +0100

    layout for userpage ./membersonly.index.js

--- pages/membersonly/index.js
+++ pages/membersonly/index.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React from 'react'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
+import styles from '../../styles/User.module.css'
 
 const members = () => {
-  return <h1>Your Day Plan!</h1>;
-};
+	return (
+		<div className={styles.main}>
+			<div className={styles.dayplan_title}>
+				<h1>Your saved dayplans:</h1>
+			</div>
+			<div className={styles.dayplan_container_main}>
+				{/* Map though saved dayplan here */}
+				<div className={styles.dayplan_container}>
+					{/* Mapping ends here */}
+				</div>
+			</div>
+			<div className={styles.liked_title}>
+				<h1>Your liked activities:</h1>
+			</div>
+			<div className={styles.liked_container_main}>
+				{/* Map though liked activities here */}
+				<div className={styles.liked_container}>
+        {/* Mapping ends here */}
+        </div>/
+			</div>
+		</div>
+	)
+}
 
-export default members;
+export default members
 
-export const getServerSideProps = withPageAuthRequired ();
+export const getServerSideProps = withPageAuthRequired()
